feb0552a5c2fac047acae38562c53096e7918b3c
Piotr Betkier <pbetkier@gmail.com>
2020-12-01 20:52:55 +0100

    adds day1

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,5 +1,16 @@
-fun day1(input: List<Int>) = 1
+@file:Suppress("UnstableApiUsage")
+
+import com.google.common.collect.Sets
+
+fun day1a(input: List<Int>) = day1(input, 2)
+
+fun day1b(input: List<Int>) = day1(input, 3)
+
+private fun day1(input: List<Int>, size: Int) = Sets.combinations(input.toSet(), size)
+            .firstOrNull { it.sum() == 2020 }
+            ?.reduce { a, x -> a * x } ?: -1
 
 fun main(args: Array<String>) {
-    println(day1(loadInts("day1a")))
+    println(day1a(loadInts("day1")))
+    println(day1b(loadInts("day1")))
 }
--- src/test/kotlin/Day1Test.kt
+++ src/test/kotlin/Day1Test.kt
@@ -4,7 +4,7 @@ class Day1Test {
 
     @Test
     fun `passes example`() {
-        val result = day1(loadInts("day1a-example"))
+        val result = day1a(loadInts("day1a-example"))
         assertEquals(514579, result)
     }
 }
